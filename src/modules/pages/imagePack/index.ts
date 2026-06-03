f8d1ab08ad146a0908843d4ec40dd8e4f2bb2faf
maoking <secretxierluo@gmail.com>
2026-06-03 22:44:48 +0800

    fix(imagePack): 修正 index.ts 导出分类
    ImagePackFileReader / ImagePackRenderer / RenderImagePackPlanInput /
    RenderImagePackPlanResult 实际定义在 imagePackRenderer.ts,之前误放到
    imagePackItemResolver 的 re-export 下,导致 typecheck 报错
    "Module has no exported member"。把 renderer 端类型从 imagePackRenderer 导出。

--- src/modules/pages/imagePack/index.ts
+++ src/modules/pages/imagePack/index.ts
@@ -1,1 +1,26 @@
 export { createImagePackPlan, suggestImagePackOutputPath } from "./imagePackPlanner";
+export {
+  createImagePackItemResolver,
+  detectImagePackSourceKind,
+  readImageDimensions,
+  resolveImagePackItems,
+  type ImageDimensions,
+  type ImagePackItemResolver,
+  type ResolveImageItemInput,
+  type ResolvePdfPageItemInput,
+  type ResolveImagePackSourcesInput,
+} from "./imagePackItemResolver";
+export {
+  createImagePackRenderer,
+  type ImagePackFileReader,
+  type ImagePackRenderer,
+  type RenderImagePackPlanInput,
+  type RenderImagePackPlanResult,
+} from "./imagePackRenderer";
+export { createImagePackExecutor } from "./imagePackExecutor";
+export type {
+  ImagePackExecutionInput,
+  ImagePackExecutionResult,
+  ImagePackExecutor,
+  ImagePackExecutorOptions,
+} from "./imagePackExecutor";
